0f808c18d0e5845e188018b2b17100dc4c0955f1
Leon Lux <leonlux@icloud.com>
2024-11-08 10:36:29 +0100

    Update +page.svelte (#25)

--- src/frontend/hosting/src/routes/+page.svelte
+++ src/frontend/hosting/src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import { Alert } from 'flowbite-svelte';
 </script>
 
-<div class="p-8">
+<div class="p-6">
   <Alert>
     <span class="font-medium">Info alert!</span>
     Change a few things up and try submitting again.
